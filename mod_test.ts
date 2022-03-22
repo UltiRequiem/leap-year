645f17cfec2150e554099de6c13a3e04c77a3398
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-22 12:20:40 -0500

    :memo: Update readme

--- mod_test.ts
+++ mod_test.ts
@@ -1,15 +1,5 @@
-import { assert } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { isLeapYear } from "./mod.ts";
-
-Deno.test("Invalid Input ", () => {});
 
 Deno.test("Detects Leap Years", () => {
   console.log(isLeapYear(2014));
-});
-
-Deno.test("Detects Leap Years", () => {
-  for (let i = 1890; i < 2030; ++i) {
-    if (isLeapYear(i)) {
-    }
-  }
 });

01027d2869f0968031d65e0474c9779df81162bd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-22 12:33:32 -0500

    :white_check_mark: Add tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,27 @@
 import { isLeapYear } from "./mod.ts";
 
-Deno.test("Detects Leap Years", () => {
-  console.log(isLeapYear(2014));
+import {
+  assertThrows,
+  assertEquals,
+  assert,
+} from "https://deno.land/std@0.128.0/testing/asserts.ts";
+
+Deno.test("Main", () => {
+  assertEquals(typeof isLeapYear(), "boolean");
+
+  assert(!isLeapYear(2000));
+  assert(isLeapYear(2020));
+  assert(isLeapYear(2016));
+
+  // My birthday :D
+  assert(!isLeapYear("6 October 2006 00:07:00 GMT"));
+});
+
+Deno.test("Handles Invalid Input", () => {
+  assertThrows(() => {
+    // @ts-ignore It should throw an error
+    isLeapYear([]);
+    // @ts-ignore It should throw an error
+    isLeapYear(new Blob());
+  }, TypeError);
 });

1436ef60b0118bf408f427ee63acbd85d8a98245
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-22 11:08:17 -0500

    :wrench: Add Node script

---
+++ scripts/node_support.ts
@@ -0,0 +1,9 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem/node_support.ts";
+
+buildPackage({
+  repoName: "@ultirequiem/leap-year",
+  description: "Check if a year is leap.",
+  homepage: "https://leap-year.js.org",
+  keywords: ["leap", "year", "date"],
+  version: "2.0.0",
+});
